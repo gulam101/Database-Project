485b7c7b775f63f2db92f513bac355d5fe19cbd7
Gulam Qasim <gfram@hotmail.co.uk>
2020-02-15 15:46:40 +0000

    Added Folder Structure

--- LoginDatabase/About.Designer.cs
+++ LoginDatabase/About.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace LoginDatabase
 {
-    partial class Login
+    partial class About
     {
         /// <summary>
         /// Required designer variable.
@@ -28,22 +28,12 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.SuspendLayout();
-            // 
-            // Login
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.components = new System.ComponentModel.Container();
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
-            this.Name = "Login";
-            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Login";
-            this.ResumeLayout(false);
-
+            this.Text = "About";
         }
 
         #endregion
     }
 }
-
